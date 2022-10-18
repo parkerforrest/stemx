eb6b378abc3c5f9844a0d392c90f018b50212440
Forrest Parker <forrestparker1994@gmail.com>
2022-10-18 10:11:30 -0400

    init

--- pages/api/getAuthenticatedUserId.js
+++ pages/api/getAuthenticatedUserId.js
@@ -1,4 +1,6 @@
 import { getAuth } from "@clerk/nextjs/server";
+global.atob = require("atob");
+global.Blob = require("node-blob");
 
 export default function handler(req, res) {
   const { sessionId, userId } = getAuth(req);
