7927bc33073d5e4d571e23588f7dc032f841ac4a
Forrest Parker <forrestparker1994@gmail.com>
2022-10-31 21:40:08 -0400

    added isPublic line to middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,7 +1,18 @@
 import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
+const publicPaths = ['/sign-in', '/sign-up', '/sign-out', '/api/auth']
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}`.replace('*s', '($|/)')))
+  )
+}
+
 export default withClerkMiddleware((req) => {
+  if (isPublic(req.nextUrl.pathname)) {
+    return NextResponse.next()
+  }
+
   const { sessionId, userId, claims, getToken, orgId } = getAuth(req)
   if (!userId) {
     const signInUrl = new URL('/sign-in', req.url)
