39cd5058b34a5e7d4fe78c8f71b58d0150f29012
Forrest Parker <forrestparker1994@gmail.com>
2022-12-14 22:00:05 -0500

    added Stem Kit unboxing video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,11 +49,12 @@ export function Hero() {
             />
             <div className="relative">
               <p className="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-                Launch your STEM career with STEMx 
+                Launch your STEM career with STEMx
               </p>
               <p className="mt-3 text-2xl tracking-tight text-slate-400">
-                Everything you need to build real solutions is at your fingertips:
-                Stem Kits, Tutorials, and a Skill Tree to track your progress towards a scholarship and internship.
+                Everything you need to build real solutions is at your
+                fingertips: Stem Kits, Tutorials, and a Skill Tree to track your
+                progress towards a scholarship and internship.
               </p>
               {/* <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
                 <Button href="/">Get started</Button>
@@ -91,7 +92,15 @@ export function Hero() {
               <div className="relative rounded-2xl bg-[#0A101F]/80 ring-1 ring-white/10 backdrop-blur">
                 {/* <div className="absolute -top-px left-20 right-11 h-px bg-gradient-to-r from-sky-300/0 via-sky-300/70 to-sky-300/0" />
                 <div className="absolute -bottom-px left-11 right-20 h-px bg-gradient-to-r from-blue-400/0 via-blue-400 to-blue-400/0" /> */}
-                <iframe width="600" height="315" src="https://www.youtube.com/embed/OOTQihLqgcc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe
+                  width="600"
+                  height="315"
+                  src="https://www.youtube.com/embed/W2I-ju4nI9k"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
                 {/* <div className="pl-4 pt-4">
                   <TrafficLightsIcon className="h-2.5 w-auto stroke-slate-500/30" />
                   <div className="mt-4 flex space-x-2 text-xs">
